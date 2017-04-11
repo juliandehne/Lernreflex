9dbcf5aed059254a33dc68e370f597a362586eac
julian.dehne@gmail.com <uzuzjmd12>
2017-04-11 09:41:47 +0200

    added inquiry create window

--- components/InquiryCreate.js
+++ components/InquiryCreate.js
@@ -1,22 +1,67 @@
 import React, { Component } from 'react';
 
-import {
+import ReactNative, {
   TouchableHighlight,
+  TouchableOpacity,
   ListView,
   ScrollView,
   Text,
+  TextInput,
+  Platform,
   View,
+  Alert,
   NavigatorIOS,
-  Platform
+  Picker,
+  Button
 } from 'react-native';
-import {styles, Router, User, Admin, CompetenceList, Loader} from 'Lernreflex/imports';
+import KeyboardSpacer from 'react-native-keyboard-spacer';
+
+import {
+  styles,
+  Router,
+  lib,
+  Competence,
+  LearningTemplate,
+  SelectList,
+  User,
+  Loader,
+  InputScrollView
+} from 'Lernreflex/imports';
 
 class Inquiry extends Component {
-  render() {
-    return (
+
+  constructor(){
+    super();
+    this.state = {
+      text:'',
+      catchwords:[],
+      group:'',
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    //alert('A name was submitted: ' + this.state.text);
+
+    // send question to server
+
+    // route to inquiry
+  }
+
+  render(){
+    return(
       <View>
-        <Text>Hello Create Dummy2!</Text>
-        <Text>Hello Dummy3!</Text>
+      <View style={{padding: 10}}>
+       <TextInput
+         style={{height: 40}}
+         placeholder="Eine Forschungsfrage zu fragen?"
+         onChangeText={(text) => this.setState({text})}
+       />
+     </View>
+        <Button
+        title="GO!"
+        onPress={this.handleSubmit}
+        />
       </View>
     );
   }
